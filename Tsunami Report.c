3a448b17c539a829e132005ea643488185f2a3d2
Deepika V <v.deepika.u@gmail.com>
2024-12-16 16:46:20 +0530

    Create Tsunami Report.c

---
+++ Tsunami Report.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d\n%d\n%d",&x,&y,&z);
+    printf("1)Dead : %d\n",x);
+    printf("2)Injured : %d\n",y);
+    printf("3)Safe : %d\n",z);
+    printf("Please help the people who are suffering!!!");
+    return 0;
+}
