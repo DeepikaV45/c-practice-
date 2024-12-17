7071c6ff29cde174492482f0520dd4ebee53602e
Deepika V <v.deepika.u@gmail.com>
2024-12-17 16:19:29 +0530

    Create Decimal.c

---
+++ Decimal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    printf("%f\n",a);
+    printf("%.3f\n",a);
+    printf("%.2f\n",a);
+    printf("%.1f",a);
+    /*printf("%.-1f\n",a);
+    printf("%.-2f\n",a);
+    return 0;*/
+}
