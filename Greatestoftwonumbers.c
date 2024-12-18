ac879e226e2f16c04e4bede94eb2d5fbaab73666
Deepika V <v.deepika.u@gmail.com>
2024-12-18 16:03:40 +0530

    Create Greatestoftwonumbers.c

---
+++ Greatestoftwonumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    scanf("%d\n%d",&a,&b);
+    if(a<b)
+        printf("%d is greater\n",b);
+        else
+        printf("%d is greater\n",a);
+    return 0;
+}
