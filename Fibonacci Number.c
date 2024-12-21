7da2839581b1acdf9fdb43f3cbc82af5b0bd5813
Deepika V <v.deepika.u@gmail.com>
2024-12-21 23:17:52 +0530

    Create Fibonacci Number.c

---
+++ Fibonacci Number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int x=0,y=1;
+    scanf("%d",&a);
+    int z=x+y;
+    if(z==a)
+    printf("Fibonacci Number");
+    else
+    printf(" Not Fibonacci Number");
+    return 0;
+}
